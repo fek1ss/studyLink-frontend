8b4383c987a7e2f6216814628cb1c9023d190844
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-10 22:57:11 +0600

    объеденил состояние в несколько груп

--- src/components/GetFile/GetFile.jsx
+++ src/components/GetFile/GetFile.jsx
@@ -1,66 +1,75 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './styles.module.css';
 import { getQuizzes } from '../../api/quiz';
 import CardQuizz from '../CardQuizz/CardQuizz';
 import Loading from '../Loading/Loading';
 
 const GetFile = () => {
-  const [file, setFile] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
   const [switchBtn, setSwitchBtn] = useState('');
-  const [fileName, setFileName] = useState('');
-  const [numQuestions, setNumQuestions] = useState(5);
-  const [quizType, setQuizType] = useState('multiple-choice');
-  const [quizzes, setQuizzes] = useState([]);
-  const [results, setResults] = useState({});
-  const [message, setMessage] = useState('');
+  const [quizState, setQuizState] = useState({
+    quizzes: [],
+    results: {},
+    message: '',
+    loading: false,
+    switchBtn: '',
+  });
 
-  useEffect(() => {
-    if (!file) {
-      setAlertMessage('');
-      return;
-    }
+  const [quizOptions, setQuizOptions] = useState({
+    numQuestions: 5,
+    quizType: 'multiple-choice',
+  });
+
+  const [fileData, setFileData] = useState({
+    file: null,
+    fileName: '',
+    alertMessage: '',
+  });
+
+  const handleFileChange = e => {
+    const file = e.target.files[0];
+    let alertMessage = '';
+
+    if (!file) return;
 
     if (
       file.size > 10 * 1024 * 1024 ||
       file.type !== 'application/pdf'
     ) {
-      setAlertMessage('File must be a PDF and less than 10MB');
-    } else {
-      setAlertMessage('');
+      alertMessage = 'File must be a PDF and less than 10MB';
     }
-  }, [file]);
 
-  const handleFileChange = e => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
+    setFileData({ file, fileName: file.name, alertMessage });
   };
 
   const handleSubmit = async e => {
     e.preventDefault();
 
-    if (!file) {
-      setAlertMessage('Please upload a PDF file');
+    if (!fileData.file) {
+      setFileData(prev => ({
+        ...prev,
+        alertMessage: 'Please upload a PDF file',
+      }));
       return;
     }
 
     const formData = new FormData();
-    formData.append('file', file);
-    formData.append('numQuestions', numQuestions);
-    formData.append('type', quizType);
+    formData.append('file', fileData.file);
+    formData.append('numQuestions', quizOptions.numQuestions);
+    formData.append('type', quizOptions.quizType);
 
-    setLoading(true);
+    setQuizState(prev => ({ ...prev, loading: true }));
     try {
       const quizzes = await getQuizzes(formData);
 
-      setQuizzes(quizzes.questions);
+      setQuizState(prev => ({
+        ...prev,
+        quizzes: quizzes.questions,
+      }));
       console.log(quizzes);
-      setAlertMessage('Quiz generated successfully!');
     } catch (err) {
       console.error('Error generating quiz:', err);
     } finally {
-      setLoading(false);
+      setQuizState(prev => ({ ...prev, loading: false }));
     }
   };
 
@@ -78,24 +87,34 @@ const GetFile = () => {
       </div>
 
       <form onSubmit={handleSubmit} className={styles.getFile__form}>
-        <p className={styles.getFile__fileName}>{fileName}</p>
+        <p className={styles.getFile__fileName}>
+          {fileData.fileName}
+        </p>
 
         <div className={styles.getFile__actions}>
-          {file !== null && switchBtn !== 'yourself' ? (
+          {fileData.file !== null && switchBtn !== 'yourself' ? (
             <div className={styles.getFile__actionsGenerate}>
               <button
                 className={`${styles.getFile__btn} ${styles.getFile__btnAi}`}
                 type="submit"
-                disabled={loading || Boolean(alertMessage)}
+                disabled={
+                  quizState.loading ||
+                  Boolean(fileData.alertMessage)
+                }
               >
-                {loading ? 'Generating...' : 'Generate!'}
+                {quizOptions.loading ? 'Generating...' : 'Generate!'}
               </button>
               <div className={styles.getFile__quizzOptions}>
                 <select
                   className={styles.getFile__numberQuiz}
                   name="select number"
-                  onChange={e => setNumQuestions(e.target.value)}
-                  value={numQuestions}
+                  onChange={e =>
+                    setQuizOptions(prev => ({
+                      ...prev,
+                      numQuestions: e.target.value,
+                    }))
+                  }
+                  value={quizOptions.numQuestions}
                 >
                   <option>number of quizzes</option>
                   <option value="5">5</option>
@@ -106,8 +125,13 @@ const GetFile = () => {
 
                 <select
                   className={styles.getFile__typeQuiz}
-                  onChange={e => setQuizType(e.target.value)}
-                  value={quizType}
+                  onChange={e =>
+                    setQuizOptions(prev => ({
+                      ...prev,
+                      quizType: e.target.value,
+                    }))
+                  }
+                  value={quizOptions.quizType}
                 >
                   <option>type of quiz</option>
                   <option value="multiple-choose">
@@ -144,43 +168,47 @@ const GetFile = () => {
           )}
         </div>
 
-        <p style={{ color: 'green' }}>{alertMessage}</p>
+        <p style={{ color: 'orange' }}>{fileData.alertMessage}</p>
       </form>
 
       <div className={styles.quizzes}>
-        {loading && <Loading />}
-        {quizzes.map((quizz, index) => (
+        {quizState.loading && <Loading />}
+        {quizState.quizzes.map((quizz, index) => (
           <CardQuizz
+            key={index}
             questionText={quizz.questionText}
             options={quizz.options}
             correctAnswer={quizz.correctAnswer}
             onAnswer={isCorrect => {
-              setResults(prev => ({
+              setQuizState(prev => ({
                 ...prev,
-                [index]: isCorrect,
+                results: { ...prev.results, [index]: isCorrect },
               }));
             }}
           />
         ))}
 
-        {quizzes.length > 0 && (
+        {quizState.quizzes.length > 0 && (
           <button
             className={styles.getFile__resultsBtn}
             onClick={() => {
-              const totalCorrect =
-                Object.values(results).filter(Boolean).length;
-              setMessage(
-                `Test results: ${totalCorrect}/${quizzes.length}`,
-              );
+              const totalCorrect = Object.values(
+                quizState.results,
+              ).filter(Boolean).length;
+              setQuizState(prev => ({
+                ...prev, message:`Test results: ${totalCorrect}/${quizState.quizzes.length}`,
+              }));
             }}
           >
             Show results
           </button>
         )}
       </div>
-      {message && (
+      {quizState.message && (
         <div className={styles.getFile__containerResults}>
-          <p className={styles.getFile__results}>{message}</p>
+          <p className={styles.getFile__results}>
+            {quizState.message}
+          </p>
         </div>
       )}
     </div>
